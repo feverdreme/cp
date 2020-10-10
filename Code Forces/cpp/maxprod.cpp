eb7325fc0629c030540e22fad5de16d88dbd6c33
FeverDreme <jeffreyhuang08@gmail.com>
2020-10-10 14:48:40 -0400

    -stuff

---
+++ Code Forces/cpp/maxprod.cpp
@@ -0,0 +1,120 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pz;
+
+#define vec vector
+#define pub push_back
+#define pb push_back
+#define pob pop_back
+#define tup tuple
+#define mp make_pair
+#define mt make_tuple
+#define get(i,x) get<i>(x)
+#define fir first
+#define sec second
+
+//iterators
+#define rep(a) for(int i=0; i<a;i++)
+#define range(i,a,b) for(int i=a;a<b;i++)
+#define fori(i,iter) for(auto i:iter)
+
+#define F0R(i,a) for(int i = 0;i<a;i++)
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define ROF(i,a,b) for(int i=a;i>=b;i--)
+#define R0F(i,a) for(int i=a;i>=0;i--)
+
+#define OE <<endl
+#define space ' '
+// #define cout cout<<
+#define cint(n) int n;cin>>n;
+#define dispbr(n) for(auto& i:n) cout<<i<<endl;
+#define disp(n) for(auto& i:n) cout<<i<<space
+
+vi factors(int n){
+	vi factors;
+	double sqrtofn=sqrt(n);
+	int count=1;
+	while(count<=sqrtofn){
+		if(n%count==0){
+			factors.pub(count);
+			if(count*count!=n)factors.pub(n/count);
+		}
+		count++;
+	}
+	return factors;
+}
+
+template<class T>
+vec<T> filter(vec<T> iter, T delim){
+	vec<T> ret;
+	fori (i,iter) if (i != delim) ret.pub(i);
+	return ret;
+}
+
+vs ssplit(string s, char delim){
+	stringstream ss(s);
+	string token;
+	vs ret;
+	while(getline(ss,token,delim)) ret.pub(token);
+	ret = filter<string>(ret,"");
+	return ret;
+}
+
+void setIO(string filename){
+	freopen((filename + ".in").c_str(), "r" , stdin);
+	freopen((filename + ".out").c_str(), "w" , stdout);
+}
+
+ll solve(vi &arr){
+	vi nums;
+
+	rep(5){
+		int maxnum = 0;
+
+		for (auto &i : arr){
+			if (maxnum <= abs(i)) maxnum = i;
+		}
+
+		nums.pub(maxnum);
+
+		arr.erase(find(arr.begin(), arr.end(), maxnum));
+	}
+
+	ll prod = nums[0] * nums[1] * nums[2] * nums[3] * nums[4];
+
+	if (arr.size() == 0 || prod >= 0) return prod;
+
+	int maxelem = *max_element(arr.begin(), arr.end());
+	if (maxelem < 0){
+		//do something else
+	}
+}
+
+int t, n;
+
+int main() {
+	std::ios_base::sync_with_stdio(false);cin.tie(0);
+	
+	cin >> t;
+	while (t--){
+		cin >> n;
+		vi arr(n);
+		arr.empty();
+
+		rep(n) cin >> arr[i];
+		
+		cout << solve(arr) << endl;
+	}
+	
+	return 0;
+}
