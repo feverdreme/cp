f089cec5c66b54d68875b13842560cc809db1f57
FeverDreme <jeffreyhuang08@gmail.com>
2020-11-09 13:44:42 -0500

    s

--- USACO/cpp/swap.cpp
+++ USACO/cpp/swap.cpp
@@ -1,54 +1,50 @@
-// #include <iostream>
-// #include <cstdio>
-// #include <sstream>
-// #include <fstream>
-//
-// #include <vector>
-// #include <array>
-// #include <deque>
-// #include <list>
-// #include <map>
-// #include <queue>
-// #include <set>
-// #include <stack>
-// #include <vector>
-// #include <algorithm>
-// #include <bitset>
-// #include <string>
-// #include <tuple>
-// #include <utility>
-// #include <cmath>
 #include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+// #pragma GCC optimize ("O3")
+#pragma GCC target("sse4")
 
 using namespace std;
 
-typedef long long z;
-typedef unsigned long long uz;
-// typedef long l;
+typedef long long ll;
+typedef unsigned long long ull;
 typedef unsigned int ui;
-typedef double d;
-
 typedef vector<int> vi;
-typedef vector<z> vz;
+typedef vector<ll> vll;
 typedef vector<string> vs;
 typedef vector<bool> vb;
-
 typedef pair<int,int> pii;
-typedef pair<z,z> pzz;
+typedef pair<ll,ll> pz;
 
 #define vec vector
+#define pb push_back
 #define pub push_back
 #define pob pop_back
+#define tup tuple
+#define mp make_pair
+#define mt make_tuple
+#define get(i,x) get<i>(x)
+#define f first
+#define s second
 
 //iterators
-#define rep(i,a) for(int i=0; i<a;i++)
+#define rep(a) for(int i=0; i<a;i++)
 #define range(i,a,b) for(int i=a;a<b;i++)
 #define fori(i,iter) for(auto i:iter)
 
+#define F0R(i,a) for(int i = 0;i<a;i++)
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define ROF(i,a,b) for(int i=a;i>=b;i--)
+#define R0F(i,a) for(int i=a;i>=0;i--)
+
 #define OE <<endl
+#define aendl "<-\n"
+#define space ' '
+#define elif else if
 // #define cout cout<<
 #define cint(n) int n;cin>>n;
-#define dvec(v) fori(i,v){cout << i OE;}
+#define dispbr(n) for(auto& i:n) cout<<i<<endl;
+#define disp(n) for(auto& i:n) cout<<i<<space
 
 vi factors(int n){
 	vi factors;
@@ -64,15 +60,10 @@ vi factors(int n){
 	return factors;
 }
 
-//python functions
 template<class T>
 vec<T> filter(vec<T> iter, T delim){
 	vec<T> ret;
-	fori (i,iter){
-		if (i != delim){
-			ret.pub(i);
-		}
-	}
+	fori (i,iter) if (i != delim) ret.pub(i);
 	return ret;
 }
 
@@ -80,66 +71,120 @@ vs ssplit(string s, char delim){
 	stringstream ss(s);
 	string token;
 	vs ret;
-
-	while(getline(ss,token,delim)){
-		ret.pub(token);
-	}
+	while(getline(ss,token,delim)) ret.pub(token);
 	ret = filter<string>(ret,"");
 	return ret;
 }
 
-template<class T>
-void reversearr(T arr[], int start, int end)
-{
-    while (start < end)
-    {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-    }
+template<class InputIterator>
+ll arr_sum(InputIterator first, InputIterator last){
+	ll sum = 0;
+	while(first != last){
+		sum += *first;
+		first++;
+	}
+	return sum;
 }
 
-int N,a1,a2,b1,b2, MAXN = 101;
-long K;
-int arr[MAXN], darr[MAXN];
+template<class InputIterator, class T>
+InputIterator arr_remove(InputIterator first, InputIterator last, const T& val){
+	while (first != last){
+		if (*first == val) return first;
+	}
+	throw "Element does not exist\n";
+}
+
+template<class InputIterator, class T>
+vll find_all(InputIterator first, InputIterator last, const T& val){
+	vec<T> inds;
+
+	InputIterator ptr = first;
+
+	while(ptr != last){
+		if (*ptr == val) inds.pb(ptr - first);
+		ptr++;
+	}
+
+	return inds;
+}
+
+template <class T>
+void Max(T &a, const T &b)
+{
+	T *ptr = &a;
+	*ptr = max(a, b);
+}
+
+template <class T>
+void Min(T &a, const T &b)
+{
+	T *ptr = &a;
+	*ptr = min(a, b);
+}
+
+void setIO(string filename){
+	freopen((filename + ".in").c_str(), "r" , stdin);
+	freopen((filename + ".out").c_str(), "w" , stdout);
+}
+
+/*
+Time complexities
+
+O(n) = 10^8
+O(n^2) = 10^4
+O(n^3) = 200
+O(logn) = Very big number, fucking massive 10^(4 * 10^8)
+O(nlogn) = 10^7
+O(2^n) = 24
+*/
+
+/*
+7 2 2
+2 5
+3 7
+*/
+
+int n,m;
+ll k;
 
 int main() {
-	ios_base::sync_with_stdio(0); cin.tie(0);
-	freopen("swap.in","r",stdin);
-	freopen("swap.out","w",stdout);
-	cin >> N >> K >> a1 >> a2 >> b1 >> b2;
+	std::ios_base::sync_with_stdio(false);cin.tie(0);
 
-  // int arr[N], darr[N];
-	rep(i,N){
-		arr[i] = i+1;
-		darr[i] = i+1;
-	}
+	setIO("swap");
 
-	long cyclecount = 0;
+	cin >> n >> m >> k;
 
-	rep(i,K){
-		reversearr(arr,a1-1,a2-1);
-		reversearr(arr,b1-1,b2-1);
-		if (arr == darr or i == K-1){
-			cyclecount = i+1;
+	// find the cycle length
+	vec<pii> lr (m);
+	rep(m) cin >> lr[i].f >> lr[i].s;
+
+	vi arr (n);
+	rep(n) arr[i] = i+1;
+
+	vi arcpy (arr.begin(), arr.end());
+
+	ll cycle = -1;
+
+	F0R(i,k){
+		F0R(j,m) reverse(arcpy.begin() + lr[j].f - 1, arcpy.begin() + lr[j].s);
+
+		if (arcpy == arr){
+			cycle = i+1;
 			break;
-		};
-	}
-
-	if (cyclecount != K){
-		//then this is the cycle
-		rep(i,K%cyclecount){
-			reversearr(arr,a1-1,a2-1);
-			reversearr(arr,b1-1,b2-1);
 		}
 	}
 
-	fori(i,arr){
-		cout << i OE;
-	}
+	if (cycle == -1){
+		dispbr(arcpy);
+	} else {
+		k = k % cycle;
 
+		rep(k){
+			F0R(j,m) reverse(arcpy.begin() + lr[j].f - 1, arcpy.begin() + lr[j].s);
+		}
+
+		dispbr(arcpy);
+	}
 
 	return 0;
 }
