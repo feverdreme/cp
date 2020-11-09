1d36feff9ef5fbd9a7929d942bd78d95c9dda28c
FeverDreme <jeffreyhuang08@gmail.com>
2020-11-09 13:45:20 -0500

    s

---
+++ USACO/cpp/convention.cpp
@@ -0,0 +1,194 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+// #pragma GCC optimize ("O3")
+#pragma GCC target("sse4")
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pz;
+
+#define vec vector
+#define pb push_back
+#define pub push_back
+#define pob pop_back
+#define tup tuple
+#define mp make_pair
+#define mt make_tuple
+#define get(i,x) get<i>(x)
+#define fir first
+#define sec second
+
+//iterators
+#define rep(a) for(int i=0; i<a;i++)
+#define range(i,a,b) for(int i=a;a<b;i++)
+#define fori(i,iter) for(auto i:iter)
+
+#define F0R(i,a) for(int i = 0;i<a;i++)
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define ROF(i,a,b) for(int i=a;i>=b;i--)
+#define R0F(i,a) for(int i=a;i>=0;i--)
+
+#define OE <<endl
+#define aendl "<-\n"
+#define space ' '
+#define elif else if
+// #define cout cout<<
+#define cint(n) int n;cin>>n;
+#define dispbr(n) for(auto& i:n) cout<<i<<endl;
+#define disp(n) for(auto& i:n) cout<<i<<space
+
+vi factors(int n){
+	vi factors;
+	double sqrtofn=sqrt(n);
+	int count=1;
+	while(count<=sqrtofn){
+		if(n%count==0){
+			factors.pub(count);
+			if(count*count!=n)factors.pub(n/count);
+		}
+		count++;
+	}
+	return factors;
+}
+
+template<class T>
+vec<T> filter(vec<T> iter, T delim){
+	vec<T> ret;
+	fori (i,iter) if (i != delim) ret.pub(i);
+	return ret;
+}
+
+vs ssplit(string s, char delim){
+	stringstream ss(s);
+	string token;
+	vs ret;
+	while(getline(ss,token,delim)) ret.pub(token);
+	ret = filter<string>(ret,"");
+	return ret;
+}
+
+template<class InputIterator>
+ll arr_sum(InputIterator first, InputIterator last){
+	ll sum = 0;
+	while(first != last){
+		sum += *first;
+		first++;
+	}
+	return sum;
+}
+
+template<class InputIterator, class T>
+InputIterator arr_remove(InputIterator first, InputIterator last, const T& val){
+	while (first != last){
+		if (*first == val) return first;
+	}
+	throw "Element does not exist\n";
+}
+
+template<class InputIterator, class T>
+vll find_all(InputIterator first, InputIterator last, const T& val){
+	vec<T> inds;
+
+	InputIterator ptr = first;
+
+	while(ptr != last){
+		if (*ptr == val) inds.pb(ptr - first);
+		ptr++;
+	}
+
+	return inds;
+}
+
+template <class T>
+void Max(T &a, const T &b)
+{
+	T *ptr = &a;
+	*ptr = max(a, b);
+}
+
+template <class T>
+void Min(T &a, const T &b)
+{
+	T *ptr = &a;
+	*ptr = min(a, b);
+}
+
+void setIO(string filename){
+	freopen((filename + ".in").c_str(), "r" , stdin);
+	freopen((filename + ".out").c_str(), "w" , stdout);
+}
+
+/*
+Time complexities
+
+O(n) = 10^8
+O(n^2) = 10^4
+O(n^3) = 200
+O(logn) = Very big number, fucking massive 10^(4 * 10^8)
+O(nlogn) = 10^7
+O(2^n) = 24
+*/
+
+/*
+6 3 2
+1 1 10 14 4 3
+*/
+
+int N, M, C;
+int t[100000];
+
+bool pos(int wait)
+{
+	int wagons = 1;
+	int firstArrival = t[0];
+	int firstIndex = 0;
+	for (int i = 1; i < N; i++)
+	{
+		if (t[i] - firstArrival > wait || i + 1 - firstIndex > C)
+		{
+			wagons += 1;
+			firstArrival = t[i];
+			firstIndex = i;
+		}
+	}
+	return (wagons <= M);
+}
+
+int binSearch(int low, int high)
+{
+	if (low == high)
+		return low;
+	if (low + 1 == high)
+	{
+		if (pos(low))
+			return low;
+		return high;
+	}
+	int mid = (low + high) / 2;
+	if (pos(mid))
+		return binSearch(low, mid);
+	else
+		return binSearch(mid + 1, high);
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);cin.tie(0);
+	setIO("convention");
+
+	cin >> N >> M >> C;
+	for (int i = 0; i < N; i++)
+		cin >> t[i];
+	sort(t, t + N);
+	cout << binSearch(0, 1000000000) << '\n';
+
+	return 0;
+}
